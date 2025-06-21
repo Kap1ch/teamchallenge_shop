b0c02e91956ff4c6c23338fd1901369e4b07efbb
andry <gorpynchenko79@gmail.com>
2025-06-21 08:37:37 +0300

    fix static

--- backend/start.sh
+++ backend/start.sh
@@ -3,5 +3,8 @@
 echo "🔄 Running migrations..."
 python manage.py migrate
 
+echo "🖼️ Running collectstatics..."
+python manage.py collectstatic --noinput
+
 echo "🚀 Starting Django server..."
 gunicorn store.wsgi
