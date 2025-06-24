89e4da45fc09a20536d4ce5324636cc2dbba5709
andry <gorpynchenko79@gmail.com>
2025-06-24 19:04:15 +0300

    add superuser

--- backend/start.sh
+++ backend/start.sh
@@ -3,6 +3,9 @@
 echo "🔄 Running migrations..."
 python manage.py migrate
 
+echo "👑 Creating superuser (if not exists)..."
+python manage.py createsuperuser --noinput || true
+
 echo "🖼️ Running collectstatics..."
 python manage.py collectstatic --noinput
 
