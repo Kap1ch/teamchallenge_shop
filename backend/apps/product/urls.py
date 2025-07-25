1bd2608ca6e379d709765abbbf855889390b6d61
andry <gorpynchenko79@gmail.com>
2025-07-25 15:02:25 +0300

    fix urls.py

--- backend/apps/product/urls.py
+++ backend/apps/product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductAllView, ProductFilterOptionsView, NewArrivalProductView, product_filter_opt
+from .views import ProductDetailView, ProductAllView, NewArrivalProductView, product_filter_opt
 
 app_name = 'product_api'
 urlpatterns = [
@@ -8,7 +8,6 @@ urlpatterns = [
     path('new_arrival/', NewArrivalProductView.as_view(), name='new_arrival-product'),
     path('category/<uuid:category_id>/', ProductAllView.as_view(), name='all-category-products'),
     path('subcategory/<uuid:subcategory_id>/', ProductAllView.as_view(), name='all-subcategory-products'),
-    # path('filters/', ProductFilterOptionsView.as_view(), name='product-filter-options'),
     path('filters/', product_filter_opt, name='product-filter-options'),
     path('<str:slug>/', ProductDetailView.as_view(), name='product-detail'),
 
