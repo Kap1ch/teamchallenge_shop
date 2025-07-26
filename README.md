900893f50005ef0e2a7472ec4b54a87b5f18ff14
andry <gorpynchenko79@gmail.com>
2025-07-27 00:24:52 +0300

    fix color filter

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# teamchallenge_shop
+# HvoyaAPI
+
+## Start project
+
+#### Clone repository
+
+`git clone https://github.com/Kap1ch/teamchallenge_shop.git`
+
+#### Create new environment
+
+`python -m venv venv `
+
+#### activate environment
+
+On Windows
+
+`venv\Scripts\activate`
+
+MacOS & Linux
+
+`source venv/bin/activate`
+
+#### Install packages
+
+`pip install -r requirements.txt`
+
+### Create an .env file using the .env.sample example 
+#### Start migrations
+
+`python backend/manage.py migrate`
+
+#### Run server
+
+`python backend/manage.py runserver`
+
+
--- backend/apps/product/filters.py
+++ backend/apps/product/filters.py
@@ -4,12 +4,15 @@ from .models import Product
 from apps.core.filters import CharInFilter
 
 
-
 class ProductFilter(filters.FilterSet):
     subcategory = filters.CharFilter(field_name='subcategory__slug', lookup_expr='iexact')
-    color = CharInFilter(field_name='productcolors__color__name', lookup_expr='in')
+    color = CharInFilter(method='filter_by_color')
     material = CharInFilter(field_name='materials__name', lookup_expr='in')
     price = filters.RangeFilter()
+
     class Meta:
         model = Product
         fields = ['subcategory', 'color', 'material', 'price']
+
+    def filter_by_color(self, queryset, name, value):
+        return queryset.filter(productcolors__color__name__in=value).distinct()
